4769cd22c6e9b6824c68245b858ea8da5a75e284
S-Vladislav <developerdev95@gmail.com>
2015-11-25 14:51:47 +0200

    fix bug profile, fixed change for schedule

--- client/app/teams/teams.schedule/teams.schedule.controller.js
+++ client/app/teams/teams.schedule/teams.schedule.controller.js
@@ -1,10 +1,20 @@
 'use strict';
 
 angular.module('abroadathletesApp')
-    .controller('ScheduleCtrl', function ($scope, User) {
+    .controller('ScheduleCtrl', function ($scope, User, Game) {
 
         $scope.showInfo = function(data) {
             console.log(data);
-        }
+        };
+
+        User.get().$promise.then(function (me) {
+            $scope.user = me;
+
+            Game.getGames({id: me._id}).$promise.then(function (games) {
+                $scope.games = games;
+            });
+            console.log($scope.games);
+        });
+
 
 });
