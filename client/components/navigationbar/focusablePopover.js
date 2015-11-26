ad634d53dfed4f5749cf73fdec6e31578a673faa
Evgeniy Sukhov <evgeniy.sukhov@chisw.com>
2015-11-26 19:26:47 +0200

    Rewrite popovers

--- client/components/navigationbar/focusablePopover.js
+++ client/components/navigationbar/focusablePopover.js
@@ -1,31 +1,38 @@
 angular.module('abroadathletesApp')
   .directive("focusablePopover",
     function ($timeout) {
+
+      var _hide = function (e) {
+        var target = e.target;
+        while (target != document.body) {
+          if (target.hasAttribute('popover-title') || target.hasAttribute('popover-placement')) return;
+          target = target.parentNode;
+        }
+        var popovers = document.querySelectorAll('.popover');
+        _.forEach(popovers, function( popover ){
+          angular.element(popover).scope().$parent.isOpen = false;
+          angular.element(popover).scope().$parent.$apply();
+        });
+      };
+
+      angular.element("body").on("click", _hide);
+
       return {
         restrict: "EAC",
         link: function (scope, element, attrs) {
-          var _hide = function () {
-            var popovers = document.querySelectorAll('.popover');
-            _.forEach(popovers, function( popover ){
-              angular.element(popover).scope().$parent.isOpen = false;
-              angular.element(popover).scope().$parent.$apply();
-            });
-          };
-
           // Stop propagation when clicking inside popover.
           element.on("click", function (event) {
             event.stopPropagation();
           });
 
-          // Hide when clicking outside.
+          /*// Hide when clicking outside.
           $timeout(function () {
-            angular.element("body").one("click", _hide);
-          }, 0);
+          }, 0);*/
 
           // Safe remove.
-          scope.$on("$destroy", function () {
+          /*scope.$on("$destroy", function () {
             angular.element("body").off("click", _hide);
-          });
+          });*/
         }
       };
     }
