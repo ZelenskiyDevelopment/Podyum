ff49e64be88c17944bed48751b34e7cda2587443
S-Vladislav <developerdev95@gmail.com>
2015-11-27 23:38:50 +0200

    1. Final makeup manage roster page 2. Began add functional for add athlete

--- client/app/manageRoster/manageRoster.controller.js
+++ client/app/manageRoster/manageRoster.controller.js
@@ -1,138 +1,166 @@
 'use strict';
 
 angular.module('abroadathletesApp')
-  .controller('ManageRosterCtrl', function ($scope, User) {
+    .controller('ManageRosterCtrl', function ($scope, User) {
 
-        console.log('init');
-    User.get().$promise.then(function (me) {
-      $scope.user = me;
-      $scope.myPlayers = $scope.user.assigned.filter(function (assignedUser) {
-        return assignedUser.user.kind === 'player' && assignedUser.isPresent;
-      });
+        $scope.templates =
+            [
+                { name: 'blocks', url: 'app/manageRoster/blocks.html', viewBlocks:true},
+                { name: 'lines', url: 'app/manageRoster/lines.html',  viewLines:true}
+            ];
+        $scope.template = $scope.templates[0];
 
-      $scope.myCoaches = $scope.user.assigned.filter(function (assignedUser) {
-        return assignedUser.user.kind === 'coach' && assignedUser.isPresent;
-      });
+        User.get().$promise.then(function (me) {
+            $scope.user = me;
+            $scope.myPlayers = $scope.user.assigned.filter(function (assignedUser) {
+                return assignedUser.user.kind === 'player' && assignedUser.isPresent;
+            });
 
-      if(me.managesRoster.length > 0) {
-        User.getUserManagesRoster({id: me._id}).$promise.then(function(user) {
-          $scope.myManagedRoster = user.managesRoster;
-        });
-      }
+            $scope.myCoaches = $scope.user.assigned.filter(function (assignedUser) {
+                return assignedUser.user.kind === 'coach' && assignedUser.isPresent;
+            });
 
-      $scope.sortChoice = 'user.player.lastName';
-
-      $scope.changeSort = function(type) {
-        if(type === $scope.sortChoice) {
-          $scope.sortChoice = '-'+$scope.sortChoice;
-        }
-        else {
-          $scope.sortChoice = type;
-        }
-      };
-
-      if(me.kind ==="team") {
-        User.getUserRosterAdmins({id: me._id}).$promise.then(function (user) {
-          $scope.myRosterAdmins = user.rosterAdmins;
-        });
-        User.getAssignRequests().$promise.then(function (users){
-          $scope.assignRequests = _.map(users, function(user){
-            return {
-              firstName: user.user[user.user.kind].firstName,
-              lastName: user.user[user.user.kind].lastName,
-              name: user.user[user.user.kind].name,
-              profilePhoto : user.user.profilePhoto,
-              dateFrom: new Date(user['dateFrom']),
-              dateTo: new Date(user['dateTo']),
-              isPresent: user['isPresent'],
-              position: user['position'],
-              _id: user.user._id
+            if (me.managesRoster.length > 0) {
+                User.getUserManagesRoster({id: me._id}).$promise.then(function (user) {
+                    $scope.myManagedRoster = user.managesRoster;
+                });
             }
-          });
-          $scope.presentRequests = $scope.assignRequests.filter(function (user) {
-              return user.isPresent;
-          });
-          $scope.pastRequests = $scope.assignRequests.filter(function (user) {
-            return !user.isPresent;
-          });
-        });
-      }
 
-      $scope.revokeRosterAdmin = function(ID, ind) {
-        var data= {id:ID};
-        $scope.myRosterAdmins.splice(ind,1);
-        User.revokeRosterAdmin({data: data});
-      };
+            $scope.switch = function (type) {
 
-      $scope.removeFromTeam = function(index, ID) {
-        if ($scope.user.kind === "team") {
-          $scope.myPlayers.splice(index, 1);
-          User.removeFromTeam({idChild: $scope.user._id, idParent: ID, isPresent: true});
-        }
-        else {
-          $scope.myPlayers.splice(index,1);
-          User.removeFromTeam({idChild: $scope.user.managesRoster[$scope.chosenTeam], idParent: ID, isPresent: true});
-        }
-      };
+                var template = findElement($scope.templates,'name',type);
 
-      $scope.chooseTeamToManage = function(ind) {
-        $scope.chosenTeam = ind;
-        User.getUserByTeam({id: $scope.myManagedRoster[ind]._id}).$promise.then(function (user) {
-          $scope.myPlayers = user.assigned.filter(function (assignedUser) {
-            return assignedUser.user.kind === 'player' && assignedUser.isPresent;
-          });
-        });
-        User.getAssignRequestsAsAdmin({id: $scope.myManagedRoster[ind]._id}).$promise.then(function (users) {
-          $scope.assignRequests = _.map(users, function(user){
-            return {
-              firstName: user.user[user.user.kind].firstName,
-              lastName: user.user[user.user.kind].lastName,
-              name: user.user[user.user.kind].name,
-              profilePhoto : user.user.profilePhoto,
-              dateFrom: new Date(user['dateFrom']),
-              dateTo: new Date(user['dateTo']),
-              isPresent: user['isPresent'],
-              position: user['position'],
-              _id: user.user._id
+                $scope.template = template;
+
+            };
+
+            $scope.sortChoice = 'user.player.lastName';
+
+            $scope.changeSort = function (type) {
+                if (type === $scope.sortChoice) {
+                    $scope.sortChoice = '-' + $scope.sortChoice;
+                }
+                else {
+                    $scope.sortChoice = type;
+                }
+            };
+
+            if (me.kind === "team") {
+                User.getUserRosterAdmins({id: me._id}).$promise.then(function (user) {
+                    $scope.myRosterAdmins = user.rosterAdmins;
+                });
+                User.getAssignRequests().$promise.then(function (users) {
+                    $scope.assignRequests = _.map(users, function (user) {
+                        return {
+                            firstName: user.user[user.user.kind].firstName,
+                            lastName: user.user[user.user.kind].lastName,
+                            name: user.user[user.user.kind].name,
+                            profilePhoto: user.user.profilePhoto,
+                            dateFrom: new Date(user['dateFrom']),
+                            dateTo: new Date(user['dateTo']),
+                            isPresent: user['isPresent'],
+                            position: user['position'],
+                            _id: user.user._id
+                        }
+                    });
+                    $scope.presentRequests = $scope.assignRequests.filter(function (user) {
+                        return user.isPresent;
+                    });
+                    $scope.pastRequests = $scope.assignRequests.filter(function (user) {
+                        return !user.isPresent;
+                    });
+                });
             }
-          });
-          $scope.presentRequests = $scope.assignRequests.filter(function (user) {
-            return user.isPresent;
-          });
-          $scope.pastRequests = $scope.assignRequests.filter(function (user) {
-            return !user.isPresent;
-          });
+
+            $scope.revokeRosterAdmin = function (ID, ind) {
+                var data = {id: ID};
+                $scope.myRosterAdmins.splice(ind, 1);
+                User.revokeRosterAdmin({data: data});
+            };
+
+            $scope.removeFromTeam = function (index, ID) {
+                if ($scope.user.kind === "team") {
+                    $scope.myPlayers.splice(index, 1);
+                    User.removeFromTeam({idChild: $scope.user._id, idParent: ID, isPresent: true});
+                }
+                else {
+                    $scope.myPlayers.splice(index, 1);
+                    User.removeFromTeam({idChild: $scope.user.managesRoster[$scope.chosenTeam], idParent: ID, isPresent: true});
+                }
+            };
+
+            $scope.chooseTeamToManage = function (ind) {
+                $scope.chosenTeam = ind;
+                User.getUserByTeam({id: $scope.myManagedRoster[ind]._id}).$promise.then(function (user) {
+                    $scope.myPlayers = user.assigned.filter(function (assignedUser) {
+                        return assignedUser.user.kind === 'player' && assignedUser.isPresent;
+                    });
+                });
+                User.getAssignRequestsAsAdmin({id: $scope.myManagedRoster[ind]._id}).$promise.then(function (users) {
+                    $scope.assignRequests = _.map(users, function (user) {
+                        return {
+                            firstName: user.user[user.user.kind].firstName,
+                            lastName: user.user[user.user.kind].lastName,
+                            name: user.user[user.user.kind].name,
+                            profilePhoto: user.user.profilePhoto,
+                            dateFrom: new Date(user['dateFrom']),
+                            dateTo: new Date(user['dateTo']),
+                            isPresent: user['isPresent'],
+                            position: user['position'],
+                            _id: user.user._id
+                        }
+                    });
+                    $scope.presentRequests = $scope.assignRequests.filter(function (user) {
+                        return user.isPresent;
+                    });
+                    $scope.pastRequests = $scope.assignRequests.filter(function (user) {
+                        return !user.isPresent;
+                    });
+                });
+            };
+
+            $scope.acceptAssignRequests = function (ID) {
+                if (ID.name) {
+                    User.acceptRecruitRequest({data: ID});
+                }
+                else {
+                    User.acceptAssignRequests({data: ID});
+                }
+                if (ID.isPresent) {
+                    _.remove($scope.presentRequests, function (assignRequest) {
+                        return (assignRequest._id === ID._id && assignRequest.dateFrom === ID.dateFrom && assignRequest.dateTo === ID.dateTo)
+                    });
+                }
+                else {
+                    _.remove($scope.pastRequests, function (assignRequest) {
+                        return (assignRequest._id === ID._id && assignRequest.dateFrom === ID.dateFrom && assignRequest.dateTo === ID.dateTo)
+                    });
+                }
+                _.remove($scope.assignRequests, function (assignRequest) {
+                    return (assignRequest._id === ID._id && assignRequest.dateFrom === ID.dateFrom && assignRequest.dateTo === ID.dateTo)
+                });
+                $scope.invitationsNumber -= 1;
+            };
+
+            $scope.rejectAssignRequests = function (ID) {
+                if (ID.name) {
+                    User.rejectRecruitRequest({data: ID});
+                }
+                else {
+                    User.rejectAssignRequests({data: ID});
+                }
+                _.remove($scope.assignRequests, function (assignRequest) {
+                    return (assignRequest._id === ID._id && assignRequest.dateFrom === ID.dateFrom && assignRequest.dateTo === ID.dateTo)
+                });
+                $scope.invitationsNumber -= 1;
+            };
+
+
         });
-      };
 
-      $scope.acceptAssignRequests = function(ID){
-        if(ID.name) {
-          User.acceptRecruitRequest({data:ID});
+        function findElement(arr, propName, propValue) {
+            for (var i=0; i < arr.length; i++)
+                if (arr[i][propName] == propValue)
+                    return arr[i];
         }
-        else {
-          User.acceptAssignRequests({data: ID});
-        }
-        if(ID.isPresent) {
-          _.remove($scope.presentRequests, function(assignRequest){return (assignRequest._id === ID._id && assignRequest.dateFrom === ID.dateFrom && assignRequest.dateTo === ID.dateTo)});
-        }
-        else {
-          _.remove($scope.pastRequests, function(assignRequest){return (assignRequest._id === ID._id && assignRequest.dateFrom === ID.dateFrom && assignRequest.dateTo === ID.dateTo)});
-        }
-        _.remove($scope.assignRequests, function(assignRequest){return (assignRequest._id === ID._id && assignRequest.dateFrom === ID.dateFrom && assignRequest.dateTo === ID.dateTo)});
-        $scope.invitationsNumber -= 1;
-      };
-
-      $scope.rejectAssignRequests = function(ID){
-        if(ID.name) {
-          User.rejectRecruitRequest({data:ID});
-        }
-        else {
-          User.rejectAssignRequests({data:ID});
-        }
-        _.remove($scope.assignRequests, function(assignRequest){return (assignRequest._id === ID._id && assignRequest.dateFrom === ID.dateFrom && assignRequest.dateTo === ID.dateTo)});
-        $scope.invitationsNumber -= 1;
-      };
-
-
     });
-  });
