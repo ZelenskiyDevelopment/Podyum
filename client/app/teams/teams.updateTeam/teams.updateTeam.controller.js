9181b25a97a617de5a5181a8f936381532b23ec9
S-Vladislav <developerdev95@gmail.com>
2015-12-04 13:05:56 +0200

    fix teams- update logo

--- client/app/teams/teams.updateTeam/teams.updateTeam.controller.js
+++ client/app/teams/teams.updateTeam/teams.updateTeam.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('abroadathletesApp')
-    .controller('UpdateTeamCtrl', function($scope, Teams, User){
+    .controller('UpdateTeamCtrl', function($scope, Teams, User, Upload){
 
         $scope.countries = [
             "Afghanistan", "Aland Islands", "Albania", "Algeria", "American Samoa", "Andorra", "Angola",
@@ -47,10 +47,51 @@ angular.module('abroadathletesApp')
 
         $scope.teamUpdate = [];
         $scope.user = [];
+        $scope.logoTeam = null;
+        $scope.croppedLogoTeam = null;
 
         $scope.colorOptions = {
             "preferredFormat": "hex"
         };
+
+        /*
+         * Select Logo Team
+         */
+        var selectLogoTeam = function(evt) {
+            var file = evt.currentTarget.files[0];
+            var reader = new FileReader();
+            reader.onload = function (evt) {
+                $scope.$apply(function($scope){
+                    $scope.logoTeam = evt.target.result;
+                });
+            };
+            reader.readAsDataURL(file);
+        };
+
+        function base64ToBlob(base64Data, contentType) {
+            contentType = contentType || '';
+            var sliceSize = 1024;
+            var byteCharacters = atob(base64Data);
+            var bytesLength = byteCharacters.length;
+            var slicesCount = Math.ceil(bytesLength / sliceSize);
+            var byteArrays = new Array(slicesCount);
+
+            for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+                var begin = sliceIndex * sliceSize;
+                var end = Math.min(begin + sliceSize, bytesLength);
+
+                var bytes = new Array(end - begin);
+                for (var offset = begin, i = 0 ; offset < end; ++i, ++offset) {
+                    bytes[i] = byteCharacters[offset].charCodeAt(0);
+                }
+                byteArrays[sliceIndex] = new Uint8Array(bytes);
+            }
+            return new Blob(byteArrays, { type: contentType });
+        }
+
+
+
+        angular.element(document.querySelector('#fileInputLogo')).on('change',selectLogoTeam);
 
         User.get().$promise.then(function (me) {
             if(!me.completed){
@@ -64,10 +105,31 @@ angular.module('abroadathletesApp')
             })
         });
 
-        $scope.updateTeam = function() {
 
-            Teams.updateTeam($scope.teamUpdate[0]).$promise.then(function(){
 
-            });
+
+
+            $scope.updateTeam = function() {
+
+
+                var logoTeam = base64ToBlob($scope.croppedLogoTeam.replace('data:image/png;base64,',''), 'image/jpeg');
+
+                Upload.upload({
+                    url: '/api/uploads/photos',
+                    file: logoTeam
+                }).progress(function (evt) {
+
+                }).success(function (dataTeam, status, headers, config) {
+
+
+                    $scope.teamUpdate[0].logoTeam = dataTeam.photo;
+                    Teams.updateTeam($scope.teamUpdate[0]).$promise.then(function(){
+
+                    });
+                });
+
+
+
+
         };
 });
